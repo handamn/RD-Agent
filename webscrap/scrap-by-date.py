37506e4ca58b986328dd496c6963d8d7033c04f6
handamn <96517339+handamn@users.noreply.github.com>
2025-01-30 21:58:53 +0700

    still error str and integer

--- webscrap/scrap-by-date.py
+++ webscrap/scrap-by-date.py
@@ -1,5 +1,7 @@
 from datetime import date
- 
+import pandas as pd
+
+
 today = date.today()
  
 print(today)
@@ -13,3 +15,16 @@ if today >= mydate :
 
 else :
     print("no")
+
+csv_file ="database\ABF Indonesia Bond Index Fund.csv"
+
+df = pd.read_csv(csv_file)
+adam = df.iloc[-1].tolist()
+adam_a = adam[0]
+adam_b = adam_a.replace("-", ", ")
+print(adam_b)
+
+date_from = date(adam_b)
+
+print(date_from)
+print(type(date_from))
