95f99aac51dbfed46b7e238c4a80ae60e2d013a2
handamn <96517339+handamn@users.noreply.github.com>
2025-04-25 11:14:56 +0700

    clean

--- RAG/extraction/new_era_v1/main/Extract copy.py
+++ RAG/extraction/new_era_v1/main/Extract copy.py
@@ -499,52 +499,44 @@ class IntegratedPdfExtractor:
             
             else:
                 segments = [
-                    (0.0, 0.36),  # Segmen 1: 0% - 50% 
-                    (0.20, 0.54),  # Segmen 2: 25% - 75%
-                    (0.38, 0.72),   # Segmen 3: 50% - 100%
-                    (0.56, 0.90),
-                    (0.74, 0.100)
+                    (0.0, 0.26),  # Segmen 1: 0% - 50% 
+                    (0.16, 0.38),  # Segmen 2: 25% - 75%
+                    (0.28, 0.50),   # Segmen 3: 50% - 100%
+                    (0.40, 0.64),
+                    (0.52, 0.76),
+                    (0.66, 0.88),
+                    (0.78, 1.00),
                 ]
-                # # Segment 1
-                # segment1_path = self.crop_image_segment(image_path, segments[0])
-                # segment1_image = Image.open(segment1_path)
-                # segment1_prompt = f"""{prompt}
-                # """
+                # Segment 1
+                segment1_path = self.crop_image_segment(image_path, segments[1])
+                segment1_image = Image.open(segment1_path)
+                segment1_prompt = f"""{prompt}
+                """
 
-                # response1 = model.generate_content([segment1_prompt, segment1_image])
-                # response1_text = response1.text
-                # parse1_json = self.extract_json_content(response1_text)
+                response1 = model.generate_content([segment1_prompt, segment1_image])
+                response1_text = response1.text
+                parse1_json = self.extract_json_content(response1_text)
 
-                # print(type(parse1_json))
-                # print(parse1_json)
 
                 # Segment 2
-                segment2_path = self.crop_image_segment(image_path, segments[1])
+                segment2_path = self.crop_image_segment(image_path, segments[2])
                 segment2_image = Image.open(segment2_path)
-                # segment2_prompt = f"""
-                # Gambar ini merupakan Potongan kedua dari sebuah gambar halaman
-                # Potongan pertama telah dilakukan ekstraksi dan disimpan dalam bentuk dictionary sebagai berikut :
-                # {parse1_json}
+                segment2_prompt = f"""
+                Gambar ini merupakan Potongan kedua dari sebuah gambar halaman
+                Potongan pertama telah dilakukan ekstraksi dan disimpan dalam bentuk dictionary sebagai berikut :
+                {parse1_json}
 
-                # tugasmu adalah :
-                # 1. melakukan ekstraksi gambar ini dengan aturan yang saya buat {prompt}
-                # 2. identifikasi kesamaan pola atau data antara hasil Potongan pertama dengan hasil ekstraksi gambar potongan kedua ini
-                # 3. jika ditemukan kesamaan pola atau data maka gabungkan sedemikian rupa sehingga menjadi data yang lengkap dengan acuna format pada aturan yang saya sebutkan pada poin nomer 1
-                # """
-
-                segment2_prompt = f"""{prompt}
+                tugasmu adalah :
+                1. melakukan ekstraksi gambar ini dengan aturan yang saya buat {prompt}
+                2. identifikasi kesamaan pola atau data antara hasil Potongan pertama dengan hasil ekstraksi gambar potongan kedua ini
+                3. jika ditemukan kesamaan pola atau data maka gabungkan sedemikian rupa sehingga menjadi data yang lengkap dengan acuna format pada aturan yang saya sebutkan pada poin nomer 1
                 """
 
                 response2 = model.generate_content([segment2_prompt, segment2_image])
                 response2_text = response2.text
-                print(response2_text)
-                print("----")
                 parse2_json = self.extract_json_content(response2_text)
 
-                print("===========")
-                print(type(parse2_json))
-                print(parse2_json)
-                print("===========")
+
 
                 return parse2_json
         
