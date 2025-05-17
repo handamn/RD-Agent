75401c6f1a1a036574854e2948625026833d4a5d
handamn <96517339+handamn@users.noreply.github.com>
2025-05-17 20:05:54 +0700

    change to extracted

--- RAG/extraction/new_era_v1/main/clean.py
+++ RAG/extraction/new_era_v1/main/clean.py
@@ -1,13 +1,13 @@
 import os
 
 # Ganti dengan path ke folder kamu
-folder_path = 'database/classified_result'
+folder_path = 'database/extracted_result'
 
 # Loop melalui semua file dalam folder
 for filename in os.listdir(folder_path):
-    if filename.endswith('_analyzed.json'):
+    if filename.endswith('_ekstraksi.json'):
         # Buat nama file baru
-        new_filename = filename.replace('_analyzed.json', '_classified.json')
+        new_filename = filename.replace('_ekstraksi.json', '_extracted.json')
 
         # Path lengkap ke file lama dan baru
         old_file = os.path.join(folder_path, filename)
